d0424e1c002a6e4fefc3a56b6f0d483540cba6de
gaborGit <gabor-vegh@hotmail.com>
2015-10-20 14:46:51 +0200

    Added some stuff

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -11,7 +11,13 @@ namespace TestApplication
         static void Main(string[] args)
         {
 
+            int i;
             System.Console.WriteLine("Hello world");
+
+            Console.WriteLine("Please enter a number: ");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}*{1}={2}", i, i, i * i);
+
         }
     }
 }
